aed2e4847928b716fb04d42643be8f99e519a80b
Jmo5896 <jmo5896@moore-clan.net>
2019-08-09 12:57:25 -0600

    update key.js next need to update readme

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: process.env.MONGODB_URI || 'YOUR MLAB CREDENTIALS GO HERE',
+    mongoURI: process.env.MONGODB_URI || 'mongodb://jmo5896:Spansyekoms22@ds157276.mlab.com:57276/heroku_q27hncx0',
     secretOrKey: 'secret'
   };
